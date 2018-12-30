ad174a64ef3f1ca478b532d2e03d9e0c0121f46e
Rik <Rikvstraaten@gmail.com>
2018-12-30 19:41:07 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Royal House</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/RoyalHouse.css') }}" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <section class="hero">
+    <div class="hero-inner">
+    <h1>Royal House</h1>
+    <h2>Rock & Roll Coverband</h2>
+    <a href="#" class="button">Book us now!</a>
+    </div>
+    </section>
+
+</body>
